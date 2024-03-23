27a390d4c22a1639cef5b365b6e34125ba4703d6
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-24 01:17:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const modal=document.querySelector(".modal");
+const buttonOpen=document.querySelector("#openModal");
+const buttonClose=document.querySelector(".close-modal");
+
+buttonOpen.addEventListener("click", ()=>{
+     modal.classList.remove("hide");
+	
+})
+buttonClose.addEventListener("click", ()=>{
+     modal.classList.add("hide");
+	
+})
